3b6d981341cd59b35ddb26d3aaaa02b4a95570c3
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2024-10-17 16:58:12 +0800

    result annotation and some modify

--- environment/printVarsByActionRun.js
+++ environment/printVarsByActionRun.js
@@ -3,3 +3,13 @@ console.log('[script Print in action]:')
 envKeys.forEach(key=>{
     console.log(key,":",process.env[key])
 })
+
+/*
+result:
+[script Print in action]:
+VAR_IN_REPO : <var in repo>
+SECRET_IN_REPO : ***
+VAR_IN_JOB1 : <var in job1>
+VAR_IN_STEP1 : undefined
+VAR_IN_RUN : <var in run>
+*/
--- environment/printVarsByDockerfile.js
+++ environment/printVarsByDockerfile.js
@@ -1,5 +1,9 @@
-const envKeys=['VAR_IN_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN','BUILD_ARG1','SECRETS1','SECRET_ENV_1']
+const envKeys=['VAR_IN_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN','BUILD_ARG1','SECRETS1','SECRET_ENV_1','VAR_IN_DOCKERFILE']
 console.log('[script Print in Dockerfile]:')
 envKeys.forEach(key=>{
     console.log(key,":",process.env[key])
 })
+/*
+result:
+
+*/
