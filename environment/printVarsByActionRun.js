08c0cacc9f427fd54e124aa60c0e81929e79b337
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2024-10-17 16:19:13 +0800

    add pass var step modify script print var

--- environment/printVarsByActionRun.js
+++ environment/printVarsByActionRun.js
@@ -1,5 +1,5 @@
-const envKeys=['VAR_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN','BUILD_ARG1','SECRETS1','SECRET_ENV_1']
-console.log('[Print in action running by script]:',process.env)
+const envKeys=['VAR_IN_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN']
+console.log('[script Print in action]:')
 envKeys.forEach(key=>{
     console.log(key,":",process.env[key])
 })
