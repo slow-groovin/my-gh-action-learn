fe86899546f08dce926718234999b1964e034dbd
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2024-10-17 16:04:01 +0800

    fix ENV setting error in Dockerfile modify print var node script

--- environment/printVarsByActionRun.js
+++ environment/printVarsByActionRun.js
@@ -1,1 +1,5 @@
+const envKeys=['VAR_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN','BUILD_ARG1','SECRETS1','SECRET_ENV_1']
 console.log('[Print in action running by script]:',process.env)
+envKeys.forEach(key=>{
+    console.log(key,":",process.env[key])
+})
--- environment/printVarsByDockerfile.js
+++ environment/printVarsByDockerfile.js
@@ -1,1 +1,5 @@
-console.log('[Print in Dockerfile running]:',process.env)
+const envKeys=['VAR_REPO', 'SECRET_IN_REPO', 'VAR_IN_JOB1', 'VAR_IN_STEP1', 'VAR_IN_RUN','']
+console.log('[Print in action running by script]:',process.env)
+envKeys.forEach(key=>{
+    console.log(key,":",process.env[key])
+})
